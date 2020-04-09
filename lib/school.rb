9c4f61cc4224f97ff72c85eb09007d8566a11ffd
Evan Smith <professionalemail@zoho.com>
2020-04-09 21:08:39 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -5,5 +5,12 @@ class School
     @name = name
     @roster = {}
   end
+  
+  def add_student(name, grade)
+    if @roster.include?(grade)==false
+      @roster[grade]
+    end
+    @roster[grade] << name
+  end
 
 end
