266575a33d43146d3f56e7ba7b38095695394d49
Evan Smith <professionalemail@zoho.com>
2020-04-09 22:06:34 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,30 @@
-# code here!
+require 'pry'
+
+class School
+  attr_accessor :name, :roster
+  
+  def initialize(name)
+    @name = name
+    @roster = {}
+  end
+  
+  def add_student(name, grade)
+    if not @roster.include?(grade)
+      @roster[grade] = []
+    end
+    @roster[grade] << name
+  end
+  
+  def grade(grade)
+    @roster[grade]
+  end
+  
+  def sort
+    done_sorted = {}
+     @roster.each do |grade, name|
+      done_sorted[grade] = name.sort
+     end
+    done_sorted
+  end
+
+end
